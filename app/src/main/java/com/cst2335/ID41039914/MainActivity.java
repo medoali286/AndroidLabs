67283f0e4e99ccb03fb8e0a06a6f7a7dd6981827
currencylayer <medoali286@gmai.com>
2023-01-31 19:04:41 +0200

    Lab 1

---
+++ app/src/main/java/com/cst2335/ID41039914/MainActivity.java
@@ -0,0 +1,15 @@
+
+package com.cst2335.ID41039914;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
